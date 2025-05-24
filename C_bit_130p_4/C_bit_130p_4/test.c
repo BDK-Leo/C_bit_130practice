513f1e83ea6d9a9bdaafe1c5455b5b0d61ff2c9b
bdkleo <mizuo6188@gmail.com>
2025-05-24 15:30:04 +0800

    bit C语言刷题第四讲

--- C_bit_130p_4/C_bit_130p_4/test.c
+++ C_bit_130p_4/C_bit_130p_4/test.c
@@ -1,2 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //bit C语言刷题第四讲
+
+//题1：时间转换
+//给定秒数，把秒转化成小时、分钟和秒
+//输入描述：一行，包括一个整数，即给定的秒数
+//输出描述：一行，包含三个整数，依次为输入整数对应的小时数，分钟数和秒数（可能为0），中间空格隔开
+//#include<stdio.h>
+//int main()
+//{
+//	int seconds, h, m, s;
+//	scanf("%d",&seconds);
+//	h = seconds / 60 / 60;
+//	m = seconds / 60 % 60;
+//	s = seconds % 60;
+//	printf("%d %d %d\n",h,m,s);
+//	return 0;
+//}
