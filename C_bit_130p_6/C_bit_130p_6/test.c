590f04af784e27cc45788243e73c79bee070e745
bdkleo <mizuo6188@gmail.com>
2025-06-01 21:02:50 +0800

    bit C语言刷题第六讲

--- C_bit_130p_6/C_bit_130p_6/test.c
+++ C_bit_130p_6/C_bit_130p_6/test.c
@@ -54,4 +54,34 @@
 //}
 
 
-//题3：
+//题3：衡量人体胖瘦程度
+//BMI<18.5(偏瘦) >=18.5&&<=23.9(正常) >23.9&&<=27.9(过重) >27.9(肥胖)
+//输入描述：多组输入，每一行包括两个整数，分别为体重公斤和身高厘米
+//输出描述：输出人体胖瘦程度
+//#include<stdio.h>
+//int main()
+//{
+//	float weight = 0.0;
+//	float high = 0.0;
+//	while (~scanf("%f %f",&weight,&high))
+//	{
+//		float BMI = weight / ((high / 100.0) * (high / 100.0));
+//		if (BMI < 18.5)
+//		{
+//			printf("偏瘦\n");
+//		}
+//		else if (BMI >= 18.5 && BMI <= 23.9)
+//		{
+//			printf("正常\n");
+//		}
+//		else if (BMI > 23.9 && BMI <= 27.9)
+//		{
+//			printf("过重\n");
+//		}
+//		else if (BMI > 27.9)
+//		{
+//			printf("肥胖\n");
+//		}
+//	}
+//	return 0;
+//}
