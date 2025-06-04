9eb769a09678d49fc77885113f1e8fd9a6a6b078
bdkleo <mizuo6188@gmail.com>
2025-06-04 16:40:18 +0800

    bit C语言刷题第七讲

--- C_bit_130p_7/C_bit_130p_7/test.c
+++ C_bit_130p_7/C_bit_130p_7/test.c
@@ -1,2 +1,27 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 //bit C语言刷题第七讲
+
+//题1：翻转直角三角形图案
+//输入描述：多组输入，一个整数(2~20)，表示翻转直角三角形直角边的长度，即"*"的数量，也表示输出行数
+//输出描述：输出对应三角形
+//#include<stdio.h>
+//int main()
+//{
+//	int n,i,j;
+//	while (~scanf("%d",&n))
+//	{
+//		while (n)
+//		{
+//			for (i = 0;i < n;i++)
+//			{
+//				for (j = 0;j < n;j++)
+//				{
+//					printf("* ");
+//				}
+//				n--;
+//				printf("\n");
+//			}
+//		}
+//	}
+//	return 0;
+//}
