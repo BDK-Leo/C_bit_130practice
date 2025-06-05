14480c1edd53adbcbabaca40be0128e1a235ed8f
bdkleo <mizuo6188@gmail.com>
2025-06-05 22:03:35 +0800

    bit C语言刷题第七讲

--- C_bit_130p_7/C_bit_130p_7/test.c
+++ C_bit_130p_7/C_bit_130p_7/test.c
@@ -25,3 +25,89 @@
 //	}
 //	return 0;
 //}
+ 
+ 
+
+//题2：带空格的直角三角形
+//输入描述：多组输入(2~20)
+//输出描述：输出对应三角形
+//方法一：
+//#include<stdio.h>
+//int main()
+//{
+//	int n, i, j;
+//	while(~scanf("%d",&n))
+//	{
+//		for (i = 0;i < n;i++)
+//		{
+//			for (j = 0;j < n - 1 - i;j++)
+//			{
+//				printf("  ");
+//			}
+//			for (j = 0;j <= i;j++)
+//			{
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//方法二：
+//#include<stdio.h>
+//int main()
+//{
+//	int n, i, j;
+//	while (~scanf("%d",&n))
+//	{
+//		for (i = 0;i < n;i++)
+//		{
+//			for (j = 0;j < n;j++)
+//			{
+//				if (i+j<n-1)
+//				{
+//					printf("  ");
+//				}
+//				else
+//				{
+//					printf("* ");
+//				}
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+//
+ 
+ 
+ 
+//闪电
+//#include<stdio.h>
+//int main()
+//{
+//	int n,i,j,m;
+//	while (~scanf("%d",&n))
+//	{
+//		while (n)
+//		{
+//			for (i = 0;i < n;i++)
+//			{
+//				for (j = n - i;j > 0;j--)
+//				{
+//					printf(" ");
+//				}
+//				for (m = 1; m <= n; m++)
+//				{
+//					printf("*");
+//				}
+//				printf("\n");
+//			}
+//			n--;
+//		}
+//	}
+//	return 0;
+//}
