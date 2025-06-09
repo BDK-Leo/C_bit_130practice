191255bc8f52df0eec72e3d507167d169cdc1e61
bdkleo <mizuo6188@gmail.com>
2025-06-09 21:41:00 +0800

    bit C语言刷题第八讲

--- C_bit_130p_8/C_bit_130p_8/test.c
+++ C_bit_130p_8/C_bit_130p_8/test.c
@@ -1,2 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //bit C语言刷题第八讲
+
+
+//题1：平均身高
+//输入描述：一行，连续输入5个身高(0.00~2.00)，空格分隔
+//输出描述：一行，输出平均身高，保留两位小数
+// 方法一：
+//#include<stdio.h>
+//int main()
+//{
+//	double high = 0.0;
+//	int i = 0;
+//	double sum = 0.0;
+//	for (i = 0;i < 5;i++)
+//	{
+//		scanf("%lf",&high);
+//		sum += high;
+//	}
+//	printf("%.2lf",sum/5.0);
+//	return 0;
+//}
+
+//方法二：
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	double arr[5] = { 0 };
+//	double sum = 0.0;
+//	for (i = 0;i < 5;i++)
+//	{
+//		scanf("%lf",&arr[i]);
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		sum += arr[i];
+//	}
+//	printf("%.2lf", sum / 5.0);
+//	return 0;
+//}
