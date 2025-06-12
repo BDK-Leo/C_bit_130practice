d5a930d247d560670dd87b21e340e62659d6e284
bdkleo <mizuo6188@gmail.com>
2025-06-12 23:41:45 +0800

    bit C语言刷题第九讲

--- C_bit_130p_9/C_bit_130p_9/test.c
+++ C_bit_130p_9/C_bit_130p_9/test.c
@@ -201,9 +201,80 @@
 // 输入一个整数序列，判断是否为有序序列
 //输入描述：第一行输入一个整数N(3<=N<=50)，第二行输入N个整数，空格分隔
 //输出描述：有序输出sorted,否则输出unsorted
-#include<stdio.h>
-int main()
-{
+//方法一：
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int arr[50] = {0};
+//	int i = 0;
+//	int j = 0;
+//	scanf("%d",&n);
+//	for (int i = 0;i < n;i++)
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	int count_1 = 0;
+//	int count_2 = 0;
+//	for (i = 0;i < n - 1;i++)
+//	{
+//		for (j = 0;j < n - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j+1])
+//			{
+//				count_1++;
+//			}
+//			else 
+//			{
+//				count_2++;
+//			}
+//		}
+//		if ((count_1 == n-1) || (count_2 == n-1))
+//		{
+//			printf("sorted\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("unsorted\n");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
 
-	return 0;
-}
+//方法二：
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[50] = { 0 };
+//	int i = 0;
+//	int flag_1 = 0;
+//	int flag_2 = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		if (i > 0)
+//		{
+//			if (arr[i] > arr[i-1])
+//			{
+//				flag_1 = 1;
+//			}
+//			else
+//			{
+//				flag_2 = 1;
+//			}
+//		}
+//	}
+//	if (flag_1 + flag_2 == 1)
+//	{
+//		printf("sorted\n");
+//	}
+//	else
+//	{
+//		printf("unsorted\n");
+//	}
+//    return 0;
+//}
