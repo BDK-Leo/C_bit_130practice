a28337315a9244e2eedf0f0cb6239f47831a22e8
bdkleo <mizuo6188@gmail.com>
2025-06-16 17:32:46 +0800

    bit C语言刷题第九讲

--- C_bit_130p_9/C_bit_130p_9/test.c
+++ C_bit_130p_9/C_bit_130p_9/test.c
@@ -409,9 +409,9 @@
 //{
 //	int n = 0;
 //	int m = 0;
-//	int arr1[100] = {0};
-//	int arr2[100] = {0};
-//	int arr3[200] = {0};
+//	int arr1[1000] = {0};
+//	int arr2[1000] = {0};
+//	int arr3[2000] = {0};
 //
 //	scanf("%d %d", &n, &m);
 //	int i = 0;
@@ -454,6 +454,56 @@
 //	for (i = 0;i < k;i++)
 //	{
 //		printf("%d ",arr3[i]);
+//	}
+//	return 0;
+//}
+
+//·½·¨Èı£º
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	int arr1[1000] = { 0 };
+//	int arr2[1000] = { 0 };
+//
+//	scanf("%d %d", &n, &m);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr1[i]);
+//	}
+//	for (i = 0; i < m; i++)
+//	{
+//		scanf("%d", &arr2[i]);
+//	}
+//	i = 0;
+//	int j = 0;
+//	int k = 0;
+//	while (i < n && j < m)
+//	{
+//		if (arr1[i] < arr2[j])
+//		{
+//			printf("%d ",arr1[i++]);
+//		}
+//		else
+//		{
+//			printf("%d ", arr2[j++]);
+//		}
+//	}
+//	if (i == n)
+//	{
+//		for (; j < m; j++)
+//		{
+//			printf("%d ",arr2[j]);
+//		}
+//	}
+//	else
+//	{
+//		for (; i < n; i++)
+//		{
+//			printf("%d ", arr1[i]);
+//		}
 //	}
 //	return 0;
 //}
