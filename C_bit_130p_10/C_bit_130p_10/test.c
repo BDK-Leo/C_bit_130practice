c7fb3989cf88f24721ba5404547d77201fc35217
bdkleo <mizuo6188@gmail.com>
2025-06-17 21:23:26 +0800

    bit C语言刷题第十讲

--- C_bit_130p_10/C_bit_130p_10/test.c
+++ C_bit_130p_10/C_bit_130p_10/test.c
@@ -67,6 +67,7 @@
 //一个包含n*m个数的整数序列，把这n*m个数按顺序规划成一个n行m列的矩阵并输出
 //输入描述：一行，输入两个整数n和m，用空格分隔，第二行包含n*m个整数（范围-231~231-1）(1≤n≤10, 1≤m≤10)
 //输出描述：输出规划后n行m列的矩阵，每个数的后面有一个空格
+// 方法一：
 //#include<stdio.h>
 //int main()
 //{
@@ -90,6 +91,27 @@
 //			printf("%d ", arr[i][j]);
 //		}
 //		printf("\n");
+//	}
+//	return 0;
+//}
+
+//方法二：
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d %d",&n,&m);
+//	int i = 0;
+//	int tmp = 0;
+//	for (i = 1;i <=n*m;i++)
+//	{
+//		scanf("%d",&tmp);
+//		printf("%d ",tmp);
+//		if (i%m == 0)
+//		{
+//			printf("\n");
+//		}
 //	}
 //	return 0;
 //}
