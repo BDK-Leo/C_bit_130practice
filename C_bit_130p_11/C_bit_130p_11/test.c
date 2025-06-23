3431a9c294c28eb34fbd325018a79b449db5ceda
bdkleo <mizuo6188@gmail.com>
2025-06-23 20:30:01 +0800

    bit C语言刷题第十一讲

--- C_bit_130p_11/C_bit_130p_11/test.c
+++ C_bit_130p_11/C_bit_130p_11/test.c
@@ -1,2 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //bit C语言刷题第十一讲
+
+
+//题目一：小乐乐与进制转换
+// 一个数转换为六进制
+//输入描述：输入一个正整数n(1<=n<=10^9)
+//输出描述：输出一行，为正整数n表示为六进制的结果
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int arr[30] = {0};
+//	scanf("%d",&n);
+//	int i = 0;
+//	while (n)
+//	{
+//		arr[i] = n % 6;
+//		i++;
+//		n /= 6;
+//	}
+//	for (i--;i>=0;i--)
+//	{
+//		printf("%d",arr[i]);
+//	}
+//	return 0;
+//}
