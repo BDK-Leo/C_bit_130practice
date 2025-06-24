ea9aa611df2129a52c696a8910d8a81f39f0a15d
bdkleo <mizuo6188@gmail.com>
2025-06-24 22:36:39 +0800

    bit C语言刷题第十一讲

--- C_bit_130p_11/C_bit_130p_11/test.c
+++ C_bit_130p_11/C_bit_130p_11/test.c
@@ -53,7 +53,6 @@
 //{
 //	long long n = 0;
 //	scanf("%lld",&n);
-//	int i = 0;
 //	long long sum = 0;
 //	sum = (1 + n) * n / 2;
 //	printf("%lld",sum);
@@ -62,6 +61,19 @@
 
 
 
-//题目三：
-//输入描述：
-//输出描述：
+//题目三：小乐乐定闹钟(只考虑时和分)
+//输入描述：输入现在的时刻以及要睡的时长K(单位：minute)
+// 输入格式：hour:minute K(0<=hour<=23,0<=minute<=59,1<=K<=10^9)
+//输出描述：输出闹钟应该设定的时刻，输出格式为标准时刻表示法
+//#include<stdio.h>
+//int main()
+//{
+//	int h = 0;
+//	int m = 0;
+//	int k = 0;
+//	scanf("%d:%d %d",&h,&m,&k);
+//	h = ((m + k) / 60 + h) % 24;
+//	m = (m + k) % 60;
+//	printf("%02d:%02d\n",h,m);
+//	return 0;
+//}
