0f04dc4985151cae6e0435bd9cb8453648177543
bdkleo <mizuo6188@gmail.com>
2025-06-29 22:09:27 +0800

    bit C语言刷题第十一讲

--- C_bit_130p_11/C_bit_130p_11/test.c
+++ C_bit_130p_11/C_bit_130p_11/test.c
@@ -362,3 +362,26 @@
 //	printf("%d\n", ret);
 //	return 0;
 //}
+
+//·½·¨¶þ£ºµÝ¹é
+//#include<stdio.h>
+//int fib(int n)
+//{
+//	if (n<=2)
+//	{
+//		return n;
+//	}
+//	else
+//	{
+//		return fib(n-1) + fib(n-2);
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d",&n);
+//	int ret = fib(n);
+//	printf("%d\n",ret);
+//	return 0;
+//}
