792f1aef49a79af315464840364cfdeca1fc88a1
bdkleo <mizuo6188@gmail.com>
2025-07-01 16:03:54 +0800

    bit C语言刷题第十二讲

---
+++ C_bit_130p_12/C_bit_130p_12/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//bit C语言刷题第十二讲
+
+
+//题目一：小乐乐查找数字
+// 给定n个整数和一个整数x，小乐乐想从n个整数中判断x出现几次
+//输入描述：共三行，第一行一个整数表示n(1<=n<=100)
+//第二行，共输入n个整数，第三行，输入一个整数，表示要查找的整数x
+//输出描述：一行，表示整数x出现的次数
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d",&n);
+//	int arr[100] = {0};
+//	int i = 0;
+//	for (i = 0;i <n;i++)
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	int x = 0;
+//	scanf("%d", &x);
+//	int num = 0;
+//	for (i = 0;i < n;i++)
+//	{
+//		if (arr[i] == x)
+//		{
+//			num++;
+//		}
+//	}
+//	printf("%d\n",num);
+//	return 0;
+//}
